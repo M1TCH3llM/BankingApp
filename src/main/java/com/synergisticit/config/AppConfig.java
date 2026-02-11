22c659230f3d3afc129881b6c48a18af6867c382
Mitchell Morgan <140917982+M1TCH3llM@users.noreply.github.com>
2026-02-11 12:31:36 -0600

    Change hibernate.hbm2ddl.auto to 'create'
    Ensure data is seeded in AWS

--- src/main/java/com/synergisticit/config/AppConfig.java
+++ src/main/java/com/synergisticit/config/AppConfig.java
@@ -69,7 +69,7 @@ public class AppConfig {
 	
 	Properties properties() {
 		Properties properties = new Properties();
-		properties.setProperty("hibernate.hbm2ddl.auto", "update");
+		properties.setProperty("hibernate.hbm2ddl.auto", "create");
 		properties.setProperty("hibernate.show_SQL", "true");
 		properties.setProperty("hibernate.hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
 		return properties;
